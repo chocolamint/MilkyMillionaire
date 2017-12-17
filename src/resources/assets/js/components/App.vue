7ed796530c924ce3bebf7099e7873999dfeb36ce
しゃるしぃ <info@angelbell.jp>
2017-12-17 18:09:24 +0900

    各プレーヤーにカードを配る

--- src/resources/assets/js/components/App.vue
+++ src/resources/assets/js/components/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="main">
     <ul>
-      <li v-for="card in cards" :key="card.id">
-        <card :card="card"></card>
-      </li>
-    </ul>
-    <ul>
       <li v-for="player in players" :key="player.name">
         <player :player="player"></player>
+        <div v-for="card in player.cards" :key="card.id">
+          <card :card="card"></card>
+        </div>
       </li>
     </ul>
   </div>
@@ -42,11 +40,32 @@ var players = [
 ];
 
 var range = n => [...Array(n).keys()].map(x => Number(x));
+var shuffle = function (arr) {
+  var i, j, temp;
+  arr = arr.slice();
+  i = arr.length;
+  if (i === 0) {
+    return arr;
+  }
+  while (--i) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+};
+
 var suits = ["♥", "♦", "♠", "♣"];
 var cards = range(13)
   .map(r => suits.map(s => new Card(s, r + 1)))
   .reduce((a, b) => a.concat(b))
   .concat(new Card(null, null, true), new Card(null, null, true));
+
+var i = 0;
+for (var card of shuffle(cards)) {
+  players[i++ % 5].cards.push(card);
+}
 
 export default {
   data() {
