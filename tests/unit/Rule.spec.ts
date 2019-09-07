a2e44841f4abe654301d43ab4b48a934cc2e4667
しゃるしぃ <info@angelbell.jp>
2019-09-07 11:05:00 +0900

    手札を捨てられるかどうかの判定のテストを追加

--- tests/unit/Rule.spec.ts
+++ tests/unit/Rule.spec.ts
@@ -1,85 +1,145 @@
-import { NormalCard, Joker } from "@/models/Card";
+import { NormalCard, Joker, Card } from "@/models/Card";
 import Rule from "@/models/Rule";
 import { expect } from "chai";
+import Stack from "@/models/Stack";
+import CardSet from "@/models/CardSet";
 
 describe("Rule.ts", () => {
 
-    it("should be ordered by weakness", () => {
-        const cards = [
-            new NormalCard("♥", 1),
-            new NormalCard("♠", 2),
-            new Joker(true),
-            new NormalCard("♠", 3),
-            new NormalCard("♦", 10),
-            new NormalCard("♣", 13),
-        ];
-        const rule = new Rule();
+    describe("sort", () => {
 
-        const sorted = rule.sort(cards, false);
+        it("should be ordered by weakness.", () => {
+            const cards = [
+                new NormalCard("♥", 1),
+                new NormalCard("♠", 2),
+                new Joker(true),
+                new NormalCard("♠", 3),
+                new NormalCard("♦", 10),
+                new NormalCard("♣", 13),
+            ];
+            const rule = new Rule();
 
-        expect(sorted).to.eql([
-            new NormalCard("♠", 3),
-            new NormalCard("♦", 10),
-            new NormalCard("♣", 13),
-            new NormalCard("♥", 1),
-            new NormalCard("♠", 2),
-            new Joker(true),
-        ]);
+            const sorted = rule.sort(cards, false);
+
+            expect(sorted).to.eql([
+                new NormalCard("♠", 3),
+                new NormalCard("♦", 10),
+                new NormalCard("♣", 13),
+                new NormalCard("♥", 1),
+                new NormalCard("♠", 2),
+                new Joker(true),
+            ]);
+        });
+
+        it("should be ordered by strongness.", () => {
+            const cards = [
+                new NormalCard("♥", 1),
+                new NormalCard("♠", 2),
+                new Joker(true),
+                new NormalCard("♠", 3),
+                new NormalCard("♦", 10),
+                new NormalCard("♣", 13),
+            ];
+            const rule = new Rule();
+
+            const sorted = rule.sort(cards, true);
+
+            expect(sorted).to.eql([
+                new Joker(true),
+                new NormalCard("♠", 2),
+                new NormalCard("♥", 1),
+                new NormalCard("♣", 13),
+                new NormalCard("♦", 10),
+                new NormalCard("♠", 3),
+            ]);
+        });
+
+        it("should be ordered by suit if rank is same.", () => {
+
+            const cards = [
+                new NormalCard("♠", 7),
+                new NormalCard("♥", 7),
+                new NormalCard("♦", 7),
+                new NormalCard("♣", 7),
+                new NormalCard("♠", 6),
+                new NormalCard("♥", 8),
+            ];
+            const rule = new Rule();
+
+            const sortedAsc = rule.sort(cards, false);
+            const sortedDesc = rule.sort(cards, true);
+
+            expect(sortedAsc).to.eql([
+                new NormalCard("♠", 6),
+                new NormalCard("♥", 7),
+                new NormalCard("♦", 7),
+                new NormalCard("♠", 7),
+                new NormalCard("♣", 7),
+                new NormalCard("♥", 8),
+            ]);
+            expect(sortedDesc).to.eql([
+                new NormalCard("♥", 8),
+                new NormalCard("♥", 7),
+                new NormalCard("♦", 7),
+                new NormalCard("♠", 7),
+                new NormalCard("♣", 7),
+                new NormalCard("♠", 6),
+            ]);
+        });
     });
 
-    it("should be ordered by strongness", () => {
-        const cards = [
-            new NormalCard("♥", 1),
-            new NormalCard("♠", 2),
-            new Joker(true),
-            new NormalCard("♠", 3),
-            new NormalCard("♦", 10),
-            new NormalCard("♣", 13),
-        ];
-        const rule = new Rule();
+    describe("canDiscard", () => {
 
-        const sorted = rule.sort(cards, true);
+        const bind = (rule: Rule, stack: Stack) => (cards: Card[]) => rule.canDiscard(stack, cards);
 
-        expect(sorted).to.eql([
-            new Joker(true),
-            new NormalCard("♠", 2),
-            new NormalCard("♥", 1),
-            new NormalCard("♣", 13),
-            new NormalCard("♦", 10),
-            new NormalCard("♠", 3),
-        ]);
-    });
+        it("should be able to discard only if stronger than top of stack.", () => {
 
-    it("should be ordered by suit if rank is same.", () => {
+            const stack = new Stack();
+            stack.cards.push(new CardSet([new NormalCard("♠", 5)], "unknown"));
+            stack.cards.push(new CardSet([new NormalCard("♥", 7)], "unknown"));
+            const rule = new Rule();
+            const canDiscard = bind(rule, stack);
 
-        const cards = [
-            new NormalCard("♠", 7),
-            new NormalCard("♥", 7),
-            new NormalCard("♦", 7),
-            new NormalCard("♣", 7),
-            new NormalCard("♠", 6),
-            new NormalCard("♥", 8),
-        ];
-        const rule = new Rule();
+            expect(canDiscard([new NormalCard("♥", 8)])).to.equal(true);
+            expect(canDiscard([new NormalCard("♠", 8)])).to.equal(true);
+            expect(canDiscard([new NormalCard("♠", 7)])).to.equal(false);
+            expect(canDiscard([new NormalCard("♥", 6)])).to.equal(false);
+        });
 
-        const sortedAsc = rule.sort(cards, false);
-        const sortedDesc = rule.sort(cards, true);
+        it("should be able to discard only if set size is same.", () => {
 
-        expect(sortedAsc).to.eql([
-            new NormalCard("♠", 6),
-            new NormalCard("♥", 7),
-            new NormalCard("♦", 7),
-            new NormalCard("♠", 7),
-            new NormalCard("♣", 7),
-            new NormalCard("♥", 8),
-        ]);
-        expect(sortedDesc).to.eql([
-            new NormalCard("♥", 8),
-            new NormalCard("♥", 7),
-            new NormalCard("♦", 7),
-            new NormalCard("♠", 7),
-            new NormalCard("♣", 7),
-            new NormalCard("♠", 6),
-        ]);
+            const stack = new Stack();
+            stack.cards.push(new CardSet([new NormalCard("♠", 5), new NormalCard("♣", 5)], "unknown"));
+            stack.cards.push(new CardSet([new NormalCard("♥", 7), new NormalCard("♦", 7)], "unknown"));
+            const rule = new Rule();
+            const canDiscard = bind(rule, stack);
+
+            expect(canDiscard([new NormalCard("♥", 8)])).to.equal(false);
+            expect(canDiscard([new NormalCard("♠", 8), new NormalCard("♣", 8), new NormalCard("♦", 8)])).to.equal(false);
+            expect(canDiscard([new NormalCard("♠", 8), new NormalCard("♣", 8)])).to.equal(true);
+        });
+
+        it("should be Joker is almighty.", () => {
+
+            const stack = new Stack();
+            stack.cards.push(new CardSet([new NormalCard("♠", 5), new NormalCard("♣", 5)], "unknown"));
+            stack.cards.push(new CardSet([new NormalCard("♥", 7), new NormalCard("♦", 7)], "unknown"));
+            const rule = new Rule();
+            const canDiscard = bind(rule, stack);
+
+            expect(canDiscard([new NormalCard("♠", 8), new Joker(true)])).to.equal(true);
+            expect(canDiscard([new NormalCard("♠", 7), new Joker(true)])).to.equal(false);
+        });
+
+        it("should be Joker pair is stronger than 2 pair.", () => {
+
+            const stack = new Stack();
+            stack.cards.push(new CardSet([new NormalCard("♥", 2), new NormalCard("♦", 2)], "unknown"));
+            const rule = new Rule();
+            const canDiscard = bind(rule, stack);
+
+            expect(canDiscard([new Joker(true), new Joker(false)])).to.equal(true);
+            expect(canDiscard([new NormalCard("♠", 2), new Joker(true)])).to.equal(false);
+        });
     });
 });
