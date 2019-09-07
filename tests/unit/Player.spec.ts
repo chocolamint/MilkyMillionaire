907b8ab53e0b47afa224fe8b4b0003993183487a
しゃるしぃ <info@angelbell.jp>
2019-09-07 12:37:19 +0900

    プレーヤーが手札をステージできるかどうかの判定のテストを追加

---
+++ tests/unit/Player.spec.ts
@@ -0,0 +1,51 @@
+import Player from "@/models/Player";
+import Rule from "@/models/Rule";
+import { NormalCard, Card } from "@/models/Card";
+import { Turn } from "@/models/Turn";
+import Stack from "@/models/Stack";
+import CardSet from "@/models/CardSet";
+import { expect } from "chai";
+import PlayerComponent from "@/components/Player/Player";
+
+describe("Player.ts", () => {
+
+    function situation(s: { stack: Card[][], deck: ReadonlyArray<Card> }) {
+
+        const rule = new Rule();
+
+        const stack = new Stack();
+        for (const cards of s.stack) {
+            stack.cards.unshift(new CardSet(cards, "unknown"));
+        }
+
+        const player = new Player("unknown");
+        player.isMyTurn = true;
+        player.currentTurn = new Turn(stack, rule, 1);
+        for (const card of s.deck) {
+            player.deal(card);
+        }
+
+        const component = new PlayerComponent();
+        component.player = player;
+        component.rule = rule;
+
+        return { player: component, deck: s.deck };
+    }
+
+    it("should be able to stage card only if can discard.", () => {
+
+        const { player, deck } = situation({
+            stack: [
+                [new NormalCard("♥", 7)],
+                [new NormalCard("♥", 5)],
+            ],
+            deck: [
+                new NormalCard("♠", 6), new NormalCard("♠", 7), new NormalCard("♠", 8),
+            ],
+        });
+
+        const actuals = deck.map(x => player.canStage(x));
+
+        expect(actuals).to.deep.equal([false, false, true]);
+    });
+});
